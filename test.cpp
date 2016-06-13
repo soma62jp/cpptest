617d686abc516d72c7ae2b60442b9b1a9d5f9395
soma62jp <soma62@hotmail.com>
2016-06-13 11:37:17 +0900

    add new file

--- test.cpp
+++ test.cpp
@@ -4,10 +4,84 @@
  * author:soma62jp
  *                  */
 
-#include <iostream>
+
 #include "test.h"
 
 using namespace std;
+
+nnet::nnet(int inputnum,int hiddennum,int outputnum):
+      inputnum(inputnum)
+     ,hiddennum(hiddennum)
+     ,outputnum(outputnum)
+     ,Eta(0.75)
+     ,Alpha(0.8)
+     ,ErrorEv(0.08)
+     ,Rlow(-0.30)
+     ,Rhigh(0.30)
+{
+  //this->inputnum=inputnum;
+  //this->hiddennum=hiddennum;
+  //this->outputnum=outputnum;
+
+  X_i = new double[inputnum];
+  X_h = new double[hiddennum];
+  X_o = new double[outputnum];
+
+  bias_h = new double[hiddennum];
+  bias_o = new double[outputnum];
+
+  // hidden*input
+  W_itoh = new double*[hiddennum];
+  for(int i=0;i<hiddennum;i++){
+    W_itoh[i] = new double[inputnum];
+  }
+
+  // output*hidden
+  W_htoo = new double*[outputnum];
+  for(int i=0;i<outputnum;i++){
+    W_htoo[i] = new double[hiddennum];
+  }
+
+
+  //initialize parameter
+  srand((unsigned int)time(NULL));
+
+  for(int i=0;i<hiddennum;i++){
+    for(int j=0;j<inputnum;j++){
+      W_itoh[i][j]=urand();
+    }
+  }
+
+  for(int i=0;i<outputnum;i++){
+    for(int j=0;j<hiddennum;j++){
+      W_htoo[i][j]=urand();
+    }
+  }
+
+
+
+}
+
+nnet::~nnet()
+{
+
+  delete [] X_i;
+  delete [] X_h;
+  delete [] X_o;
+
+  delete [] bias_h;
+  delete [] bias_o;
+  
+  for( int i=0; i<hiddennum; i++ ) {
+    delete[] W_itoh[i];
+  }
+  delete [] W_itoh;
+
+  for( int i=0; i<outputnum; i++ ) {
+    delete [] W_htoo[i];
+  }
+  delete [] W_htoo;
+}
 
 int main()
 {
--- test.h
+++ test.h
@@ -1,4 +1,38 @@
 #ifndef _TEST_H_
 #define _TEST_H_
 
+#include <iostream>
+#include <cmath>
+#include <ctime>        // time
+#include <cstdlib>      // srand,rand
+
+class nnet
+{
+ public:
+  nnet(int inputnum,int hiddennum,int outputnum);
+  ~nnet();
+
+ private:
+  double **W_itoh;  // weight input to hidden
+  double **W_htoo;  // weight hidden to output
+  double *X_i;  // X inpit
+  double *X_h;  // X hidden
+  double *X_o;  // X output
+  double *bias_h;   // bias hidden layer
+  double *bias_o;   // bias output layer
+  int inputnum;
+  int hiddennum;
+  int outputnum;
+
+  const double Eta;
+  const double Alpha;
+  const double ErrorEv;
+  const double Rlow;
+  const double Rhigh;
+
+  #define activationFunc(x) (1/(1+exp(-x))))
+  #define urand() ((double) rand()/RAND_MAX * (Rhigh - Rlow) + Rlow)
+  
+};
+
 #endif
